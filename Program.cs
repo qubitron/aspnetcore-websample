a0a4f4223e3916f55bdf8506b57ce09d927642de
Dan Taylor <dantaylo@microsoft.com>
2016-07-01 20:21:23 -0700

    Uses MVC and allows user to specify userId in URL

--- Program.cs
+++ Program.cs
@@ -1,26 +1,46 @@
 using System.Net.Http;
+using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 using Newtonsoft.Json;
 
+public class GitHubController : Controller
+{
+    public async Task<string> GitUser(string userId)
+    {
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("User-Agent", "aspnetcore-websample");
+        string result = await client.GetStringAsync("https://api.github.com/users/" + userId);
+
+        dynamic data = JsonConvert.DeserializeObject(result);
+        string name = data.name;
+
+        return "Hello " + name + "!!!1";
+    }
+}
+
 public class Startup
 {
+    // Add Framework Services
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddMvc();
+    }
+
+
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app)
     {
-        app.Run(async (context) =>
+        app.UseMvc(routes =>
         {
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.Add("User-Agent", "aspnetcore-websample");
-            string result = await client.GetStringAsync("https://api.github.com/users/qubitron");
-            
-            dynamic data = JsonConvert.DeserializeObject<dynamic>(result);
-            string name = data.name;
-
-            await context.Response.WriteAsync("Hello " + name + "!!!1");
+            routes.MapRoute(
+                name: "default",
+                template: "{action}/{userId}",
+                defaults: new { controller = "GitHub", action="GitUser", userId="qubitron" });
         });
     }
 }
