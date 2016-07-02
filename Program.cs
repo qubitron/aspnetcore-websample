94a1d1e959748eab06087273d2a1565633697001
Dan Taylor <dantaylo@microsoft.com>
2016-07-01 19:36:25 -0700

    Name is parsed from json result

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 
+using Newtonsoft.Json;
+
 public class Startup
 {
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -12,10 +14,13 @@ public class Startup
         app.Run(async (context) =>
         {
             var client = new HttpClient();
-            client.DefaultRequestHeaders.Add("User-Agent", "NetCore-Sample-App");
+            client.DefaultRequestHeaders.Add("User-Agent", "aspnetcore-websample");
             string result = await client.GetStringAsync("https://api.github.com/users/qubitron");
+            
+            dynamic data = JsonConvert.DeserializeObject<dynamic>(result);
+            string name = data.name;
 
-            await context.Response.WriteAsync(result);
+            await context.Response.WriteAsync("Hello " + name + "!!!1");
         });
     }
 }
